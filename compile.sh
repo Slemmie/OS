a68585152c58d2e9595fdb28c9ce7d24447349a6
Slemmie <thorvejeneriksen@gmail.com>
2022-02-21 17:23:12 +0100

    update compile script to include kernel

--- compile.sh
+++ compile.sh
@@ -2,6 +2,12 @@
 # requires assembly compiler 'nasm'
 # $ apt install nasm
 
+# compile kernel
+cd kernel && gcc -ffreestanding -c kernel.c -o kernel.o
+ld -o ../kernel.bin -Ttext 0x1000 kernel.o --oformat binary
+cd ..
+
+# compile boot sector
 cd boot_sector && nasm -f bin boot.asm -o ../boot.bin
 
 echo "DONE"
