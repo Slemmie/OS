c3a186da176cb0608aca330638b7cb5d2cb6dda8
Slemmie <thorvejeneriksen@gmail.com>
2022-02-21 22:10:39 +0100

    debug pr function in kernel

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,10 +1,14 @@
 // kernel written in C
 
+void pr(int id, short value) {
+	((char*)0x000b8002)[id * 2] = value;
+}
+
 void main() {
 	// sample program
 	// write a character to vid-mem
 	
-	char* video_memory_address = (char*) 0xb8000;
+	long int place = 0;
+	pr(place, 'H');
 	
-	*video_memory_address = 'X';
 }
