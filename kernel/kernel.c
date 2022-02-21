7ff6498e1e050fb9cd5a6fdf2da37e427ceb5d03
Slemmie <thorvejeneriksen@gmail.com>
2022-02-21 17:20:21 +0100

    fix typecast warning

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -4,7 +4,7 @@ void main() {
 	// sample program
 	// write a character to vid-mem
 	
-	char* video_memory_address = 0xb8000;
+	char* video_memory_address = (char*) 0xb8000;
 	
 	*video_memory_address = 'X';
 }
