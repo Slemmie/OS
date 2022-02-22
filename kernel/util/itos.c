bb1994df0af6d5901bd7dde472e78539bf8ae51b
Slemmie <thorvejeneriksen@gmail.com>
2022-02-22 02:43:00 +0100

    add support for unsigned int in itos function

--- kernel/util/itos.c
+++ kernel/util/itos.c
@@ -5,17 +5,37 @@ const char DIGIT_TO_CHAR[] = "0123456789abcdef";
 
 int itos(int value, char* buffer, int base) {
 	// check for violating possible base ranges
-	if (base < 2 || base > 16) {
+	if (base * (base < 0 ? -1 : 1) < 2 || base * (base < 0 ? -1 : 1) > 16) {
 		return 1;
 	}
 	
-	// extract digits
 	int ptr = 0;
-	do {
-		int digit = value % base;
-		buffer[ptr++] = DIGIT_TO_CHAR[digit];
-		value /= base;
-	} while (value);
+	
+	if (base > 0) { // signed mode
+		// extract digits
+		int neg = 0;
+		if (value < 0) {
+			neg = 1;
+			value = -value;
+		}
+		do {
+			int digit = value % base;
+			buffer[ptr++] = DIGIT_TO_CHAR[digit];
+			value /= base;
+		} while (value);
+		if (neg) {
+			buffer[ptr++] = '-';
+		}
+	} else { // unsigned mode
+		base = -base;
+		// extract digits
+		unsigned int uvalue = (unsigned int) value;
+		do {
+			int digit = uvalue % base;
+			buffer[ptr++] = DIGIT_TO_CHAR[digit];
+			uvalue /= base;
+		} while (uvalue);
+	}
 	
 	// reverse string
 	for (int i = 0; i < ptr / 2; i++) {
--- kernel/util/itos.h
+++ kernel/util/itos.h
@@ -3,6 +3,7 @@
 #pragma once
 
 // value to convert, buffer to store result (null terminated), base range [2, 16]
+// setting base to a negatve (abs value in range) will result in value being interpreted as unsigned
 // returns non-zero value if an error occurred
 // make sure buffer has enough room for the string representation, itoa does not alloca anything
 int itos(int value, char* buffer, int base);
