42b4d71444083c72cc28c257cdda9af7946525c0
Sofapuden <sofapudr@gmail.com>
2022-02-23 00:18:21 +0100

    Initial boot_loader

---
+++ boot_loader/boot_loader.asm
@@ -0,0 +1,15 @@
+[org 0x7c00]
+
+mov bp, 0x7c00
+mov sp, bp
+
+mov bx, TestString
+call PrintString
+
+jmp $
+
+%include "./util/print.asm"
+
+times 510 - ($-$$) db 0
+dw 0xaa55
+
---
+++ boot_loader/util/print.asm
@@ -0,0 +1,25 @@
+PrintString:
+	push ax
+	push bx
+
+	mov ah, 0x0e
+
+	.Loop:
+		cmp [bx], byte 0
+		je .Exit
+		mov al, [bx]
+		int 0x10
+		inc bx
+		jmp .Loop
+
+	.Exit:
+		pop bx
+		pop ax
+		ret
+
+TestString:
+	db "This is a test string", 0
+		
+	
+
+
