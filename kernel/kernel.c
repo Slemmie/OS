1dcc93eeaaf5bef6935abcc5a9f73c76b208c9d5
Slemmie <thorvejeneriksen@gmail.com>
2022-02-24 00:25:55 +0100

    add reverse algorithm

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -4,13 +4,5 @@
 #include "util/algorithm.h"
 
 void _start() {
-	char c1 = 'h';
-	char c2 = 'i';
-	
-	swap(c1, c2);
-	
-	*((uint_8*) 0xb8000) = (uint_8) c1;
-	*((uint_8*) 0xb8001) = (uint_8) 0x0f;
-	*((uint_8*) 0xb8002) = (uint_8) c2;
-	*((uint_8*) 0xb8003) = (uint_8) 0x0f;
+	return;
 }
--- kernel/util/algorithm.h
+++ kernel/util/algorithm.h
@@ -5,17 +5,26 @@
 #include "types.h"
 
 // return minimum of _v1 and _v2
-#define min(_v1, _v2) (_v1 < _v2 ? _v1 : _v2)
+#define min(_v1, _v2) ((_v1) < (_v2) ? (_v1 : _v2))
 // return maximum of _v1 and _v2
-#define max(_v1, _v2) (_v1 > _v2 ? _v1 : _v2)
+#define max(_v1, _v2) ((_v1) > (_v2) ? (_v1 : _v2))
 
 // return absolute value of _v
-#define abs(_v) (max(_v, -_v))
+#define abs(_v) (max((_v), -(_v)))
 
 // swaps _v1 and _v2
 #define swap(_v1, _v2) ({   \
-	typeof(_v1) __v1 = _v1; \
-	typeof(_v2) __v2 = _v2; \
-	_v1 = __v2;             \
-	_v2 = __v1;             \
+	typeof(_v1) __v1 = (_v1); \
+	typeof(_v2) __v2 = (_v2); \
+	(_v1) = __v2;             \
+	(_v2) = __v1;             \
+})
+
+// reverses array of given size
+// end has to be >= begin
+#define reverse(_begin, _end) ({ \
+	size_t __size = (_end) - (_begin); \
+	for (size_t __i = 0; __i < __size / (size_t) 2; __i++) { \
+		swap(*(_begin + __i), *(_end - __i - 1)); \
+	} \
 })
