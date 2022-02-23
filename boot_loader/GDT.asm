de674112144dd4c43a3e841a0810a6a825359b51
Slemmie <thorvejeneriksen@gmail.com>
2022-02-23 01:04:59 +0100

    add GDT + start_protected_mode proc

---
+++ boot_loader/GDT.asm
@@ -0,0 +1,31 @@
+; GDT object
+
+gdt_null_descriptor:
+	dd 0
+	dd 0
+
+gdt_code_segment:
+	dw 0xffff    ; limit
+	dw 0x0000    ; base (low)
+	db 0x00      ; base (med)
+	db 10011010b ; access byte
+	db 11001111b ; flags | limit
+	db 0x00      ; base (hig)
+
+gdt_data_segment:
+	dw 0xffff    ; limit
+	dw 0x0000    ; base (low)
+	db 0x00      ; base (med)
+	db 10010010b ; access byte
+	db 11001111b ; flags | limit
+	db 0x00      ; base (hig)
+
+gdt_end:
+
+gdt_descriptor:
+	gdt_size:
+		dw gdt_end - gdt_null_descriptor - 1
+		dd gdt_null_descriptor
+
+code_segment equ gdt_code_segment - gdt_null_descriptor
+data_segment equ gdt_data_segment - gdt_null_descriptor
--- boot_loader/extended_program.asm
+++ boot_loader/extended_program.asm
@@ -1,13 +1,50 @@
 [org 0x7e00]
 
-mov bx, ExtendedSpaceSuccess
-call PrintString
+; never return from here
+jmp enter_protected_mode
 
-jmp $
-
+%include "GDT.asm"
 %include "./util/print.asm"
 
-ExtendedSpaceSuccess:
-	db "Entered extended space successfully", 0
+; enter 32-bit protected mode
+enter_protected_mode:
+	call enable_A20
+
+	; disable interrupts
+	cli
+	
+	; load gdt
+	lgdt [gdt_descriptor]
+	
+	; set up 32-bit protected mode
+	mov eax, cr0
+	or eax, 1
+	mov cr0, eax
+	
+	; never return from here
+	jmp code_segment:start_protected_mode
+
+enable_A20:
+	in al, 0x92
+	or al, 0x02
+	out 0x92, al
+	
+	ret
+
+[bits 32]
+
+start_protected_mode:
+	mov ax, data_segment
+	mov ds, ax
+	mov ss, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	
+	; print yellow X (remove later :))
+	mov [0xb8000], byte 'X'
+	mov [0xb8001], byte 0x0e
+	
+	jmp $
 
 times 2048 - ($-$$) db 0
