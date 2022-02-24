01598aa1a9049e9e5dae579e73dac0e986523549
Slemmie <thorvejeneriksen@gmail.com>
2022-02-24 04:07:38 +0100

    add simplified vidmem API option

--- kernel/vidmem/vidmem.h
+++ kernel/vidmem/vidmem.h
@@ -4,6 +4,15 @@
 
 #include "../util/types.h"
 
+// put this define before including vidmem header to enable simplified function names as well
+#ifdef VIDMEM_SIMPLIFIED_API
+
+#define set_cursor         vidmem_set_cursor
+#define clear_screen       vidmem_clear_screen
+#define clear_screen_color vidmem_clear_screen_color
+
+#endif
+
 // set cursor position
 void vidmem_set_cursor(uint_8 row, uint_8 col);
 
