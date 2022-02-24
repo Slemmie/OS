153ee10a6dbafd039cf9f3b949e52c176a2eafad
Slemmie <thorvejeneriksen@gmail.com>
2022-02-24 03:56:45 +0100

    fix algorithm min() and max() bugs

--- kernel/util/algorithm.h
+++ kernel/util/algorithm.h
@@ -5,9 +5,9 @@
 #include "types.h"
 
 // return minimum of _v1 and _v2
-#define min(_v1, _v2) ((_v1) < (_v2) ? (_v1 : _v2))
+#define min(_v1, _v2) ((_v1) < (_v2) ? (_v1) : (_v2))
 // return maximum of _v1 and _v2
-#define max(_v1, _v2) ((_v1) > (_v2) ? (_v1 : _v2))
+#define max(_v1, _v2) ((_v1) > (_v2) ? (_v1) : (_v2))
 
 // return absolute value of _v
 #define abs(_v) (max((_v), -(_v)))
