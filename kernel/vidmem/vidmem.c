8d5c6bf500545905f7e489965111e9b22e94146c
Slemmie <thorvejeneriksen@gmail.com>
2022-02-24 03:57:24 +0100

    cap row and col in vidmem_set_cursor()

--- kernel/vidmem/vidmem.c
+++ kernel/vidmem/vidmem.c
@@ -3,6 +3,7 @@
 #include "vidmem.h"
 
 #include "../port_io.h"
+#include "../util/algorithm.h"
 
 #define _VIDMEM_ROW_CNT 25
 #define _VIDMEM_COL_CNT 80
@@ -31,7 +32,11 @@ uint_8 _vidmem_col_of(_vm_pos position) {
 // end utility (used only in vidmem.c)
 
 // set cursor position
+// caps row at _VIDMEM_ROW_CNT - 1
+// caps col at _VIDMEM_COL_CNT - 1
 void vidmem_set_cursor(uint_8 row, uint_8 col) {
+	row = min(row, _VIDMEM_ROW_CNT - 1);
+	col = min(col, _VIDMEM_COL_CNT - 1);
 	_vm_pos position = _vidmem_position_of(row, col);
 	port_byte_out(0x3d4, 0x0f);
 	port_byte_out(0x3d5, (uint_8) position);
